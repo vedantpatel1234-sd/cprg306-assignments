7e56e02bee1da7a9c8e060ef48198dcc7d3ae60f
Your Name <your-email@example.com>
2026-01-22 14:42:12 -0700

    Week 2 assignment completed

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <section>
+      <p>Your Name: Vedant Patel</p>
+      <p>
+        GitHub Repository:{" "}
+        <Link href="https://github.com/vedantpatel1234-sd/cprg306-assignments">
+          https://github.com/vedantpatel1234-sd/cprg306-assignments
+        </Link>
+      </p>
+    </section>
+  );
+}
