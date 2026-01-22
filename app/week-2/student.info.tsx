54760e070659688e62985f8e72ffc5ed6ef58ccb
Your Name <your-email@example.com>
2026-01-22 14:53:16 -0700

    update github link

--- app/week-2/student.info.tsx
+++ app/week-2/student.info.tsx
@@ -7,7 +7,7 @@ export default function StudentInfo() {
       <p>
         GitHub Repository:{" "}
         <Link href="https://github.com/vedantpatel1234-sd/cprg306-assignments">
-          https://github.com/vedantpatel1234-sd/cprg306-assignments
+          My Github Repo
         </Link>
       </p>
     </section>
