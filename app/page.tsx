ebdda4be3bef22e2a2c00f440355cf28ed591ab3
Your Name <your-email@example.com>
2026-01-29 13:37:25 -0700

    updated week-3

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function Page() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">Go to Week 2 Assignment</Link>
+      <br />
+      <Link href="/week-3">Go to Week 3 Assignment</Link>
     </main>
   );
 }
+
+
---
+++ app/week-3/page.tsx
@@ -0,0 +1,14 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-slate-100 p-6">
+      <div className="mx-auto max-w-6xl">
+        <h1 className="mb-6 text-3xl font-bold text-slate-900">
+          Shopping List
+        </h1>
+        <ItemList />
+      </div>
+    </main>
+  );
+}
